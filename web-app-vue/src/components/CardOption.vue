457bd80b1b02b2395a89b38fcfed4b6fe6ad2c51
Leonardo Silveira <sombriks@gmail.com>
2023-04-25 20:58:33 -0300

    drafts

---
+++ web-app-vue/src/components/CardOption.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card">{{ props.title }}</div>
+</template>
+
+<script setup>
+const props = defineProps(["title"])
+</script>
+
+<style scoped>
+.card {
+    border: 1px solid red;
+    min-width: 100%;
+    margin: 1em;
+    padding: 1em;
+    border-radius: 1em;
+}
+.expand {
+
+}
+</style>
