dbc18ad3ca73b8c20581286bf3513e472fd0124f
Leonardo Silveira <sombriks@gmail.com>
2023-07-03 11:47:19 -0300

    works on REDLINE-20

--- web-app-vue/src/components/categoria/lista-categorias.vue
+++ web-app-vue/src/components/categoria/lista-categorias.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>Categorias</div>
+  <ul>
+    <li v-if="!cState.store.categorias || !cState.store.categorias.length">Não há categorias para exibir</li>
+    <li v-for="cat in cState.store.categorias" :key="cat.id">{{cat.descricao}}</li>
+  </ul>
 </template>
-<script setup></script>
+<script setup>
+
+import { useCategoriaStore } from "@/stores/categoriaStore";
+import { onMounted } from "vue";
+
+const cState = useCategoriaStore()
+
+onMounted(() => {
+  cState.sincronizarCategorias()
+})
+</script>
 <style scoped></style>
--- web-app-vue/src/stores/categoriaStore.js
+++ web-app-vue/src/stores/categoriaStore.js
@@ -1,11 +1,21 @@
 import { defineStore } from 'pinia'
 import { reactive } from 'vue'
 import { getRedLine } from "@/services/redLine";
+import { listCategorias } from "@/services/api";
+import { useUserStore } from "@/stores/userStore";
 
 export const useCategoriaStore = defineStore('categoria-store', () => {
+  const uState = useUserStore()
+
+
   const store = reactive({
     categorias: getRedLine()?.categorias || []
   })
 
-  return { store }
+  const sincronizarCategorias = async () => {
+    const {id} = uState.userData
+    store.categorias = await listCategorias({id})
+  }
+
+  return { store, sincronizarCategorias }
 })
