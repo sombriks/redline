0061c3aa942cae87f483333d36d6c8d6a27e825d
Leonardo Silveira <sombriks@gmail.com>
2023-07-07 23:26:33 -0300

    fix typo

--- service-node-koa/app/config/security/middleware.mjs
+++ service-node-koa/app/config/security/middleware.mjs
@@ -15,6 +15,6 @@ export const ifAuthenticated = (ctx, next) => {
 export const contaOwnedBy = async (ctx, next) => {
   const { usuario_id, conta_id } = ctx.request.params;
   const [count] = await knex("conta").where({ usuario_id, id: conta_id }).count();
-  if (count[Object.keys(count)]) await next();
+  if (count[Object.keys(count)[0]]) await next();
   else throw new Error(`Conta ${conta_id} não pertence ao usuário ${usuario_id}`);
 };
