2b888f6ba6eeae9b6bab52cefb6ad33f0d97abbb
Leonardo Silveira <sombriks@gmail.com>
2023-07-11 21:28:46 -0300

    testing https://github.com/webfansplz/vite-plugin-vue-devtools

--- web-app-vue/vite.config.js
+++ web-app-vue/vite.config.js
@@ -2,10 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueDevtools from 'vite-plugin-vue-devtools'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VueDevtools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
