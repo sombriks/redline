cc1eff18f586eae8f354724bb2a94785fb580223
Leonardo Silveira <sombriks@gmail.com>
2024-01-26 20:23:37 -0300

    wrks on RED-65

--- service-node-koa/app/config/security/middleware.mjs
+++ service-node-koa/app/config/security/middleware.mjs
@@ -1,7 +1,7 @@
 import {knex} from "../db/index.mjs";
 import {verify} from "./encryption.mjs";
 
-export const extractDetails = ctx => {
+const extractDetails = ctx => {
   const authHeader = ctx.request.header["authorization"];
   if (!authHeader) ctx.throw(401, {message: "Missing auth header"});
   const token = authHeader.replace("Bearer ", "");
@@ -9,28 +9,25 @@ export const extractDetails = ctx => {
 }
 
 export const ifAdmin = async (ctx, next) => {
-  return await next();
-};
-
-export const ifOwner = async (ctx, next) => {
-  return await next();
+  const {admin} = extractDetails(ctx)
+  if (admin) return await next();
+  else return ctx.throw(403, "User is not an admin");
 };
 
 export const ifAuthenticated = async (ctx, next) => {
-  const details = extractDetails(ctx)
-  // console.log(details)
-  // console.log(new Date(details.exp * 1000))
+  const details = extractDetails(ctx) // id, nome, emil, admin, criacao, alteracao, iat, exp
   if (!details.iat || !details.exp)
     ctx.throw(401, {message: "Something strange with this token"})
   if (new Date().getTime() > new Date(details.exp * 1000))
     ctx.throw(401, {message: "Token expired"})
-  // ctx.throw(401, {message:"test"})
   return await next()
 };
 
 export const contaOwnedBy = async (ctx, next) => {
+  const {id} = extractDetails(ctx)
   const {usuario_id, conta_id} = ctx.request.params;
+  if (id != usuario_id) return ctx.throw(403, `Usuário logado não é o usuário informado`);
   const [count] = await knex("conta").where({usuario_id, id: conta_id}).count();
   if (count[Object.keys(count)[0]]) await next();
-  else throw new Error(`Conta ${conta_id} não pertence ao usuário ${usuario_id}`);
+  else ctx.throw(403, `Conta ${conta_id} não pertence ao usuário ${usuario_id}`);
 };
---
+++ service-node-koa/app/config/security/middleware.spec.mjs
@@ -0,0 +1,78 @@
+import chai from "chai"
+
+import * as middleware from "./middleware.mjs"
+import sinon from "sinon";
+import {sign} from "./encryption.mjs";
+import {getAdmin, resetConta} from "../../services/index.mjs";
+
+chai.should();
+
+describe("Middleware tests", () => {
+
+  it("should check if it's admin", async () => {
+    const adm = await getAdmin()
+    const {token} = sign(adm)
+    const authorization = `Bearer ${token}`
+    const ctx = {request: {header: {authorization}}, throw: sinon.fake()}
+    const next = sinon.mock()
+    next.once()
+
+    await middleware.ifAdmin(ctx, next)
+
+    next.verify()
+  })
+
+  it("should check if it's authenticated", async () => {
+    const adm = await getAdmin()
+    const {token} = sign(adm)
+    const authorization = `Bearer ${token}`
+    const ctx = {request: {header: {authorization}}, throw: sinon.fake()}
+    const next = sinon.mock()
+    next.once()
+
+    await middleware.ifAuthenticated(ctx, next)
+
+    next.verify()
+
+  })
+
+  it("should check if it owns the resource", async () => {
+    // given
+    const adm = await getAdmin()
+    const contasIds = await resetConta({usuario_id: adm.id})
+    const {token} = sign(adm)
+    const authorization = `Bearer ${token}`
+    const params = {usuario_id: adm.id, conta_id: contasIds[0].id}
+    const ctx = {request: {header: {authorization}, params}, throw: sinon.fake()}
+    const next = sinon.mock()
+    next.once()
+
+    // when
+    await middleware.contaOwnedBy(ctx, next)
+
+    // then
+    next.verify()
+  })
+
+  it("Should FAIL due missing auth header", async () => {
+    const authorization = `Bearer`
+    const ctx = {request: {header: {authorization}}, throw: sinon.mock()}
+    const next = sinon.mock()
+    next.never()
+    ctx.throw.never()
+
+    const spyable = { ifAuthenticated: middleware.ifAuthenticated}
+
+    const spy = sinon.spy(spyable, "ifAuthenticated")
+    try {
+      await spyable.ifAuthenticated(ctx, next)
+    } catch(e) {
+      chai.expect(spy.exceptions).length(1)
+    }
+
+    chai.expect(spy.called)
+    chai.expect(spy.threw())
+    ctx.throw.verify()
+    next.verify()
+  })
+})
