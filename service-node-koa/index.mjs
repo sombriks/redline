070d30d73432ef7848dedc1ffaecc868b775a487
Leonardo Silveira <sombriks@gmail.com>
2024-02-29 19:53:43 -0300

    better logger line

--- service-node-koa/index.mjs
+++ service-node-koa/index.mjs
@@ -1,9 +1,9 @@
 import { app } from "./app/main.mjs"
 import { dbMigrate } from "./app/config/db/index.mjs";
-import { cabin } from './app/config/base-logging.mjs'
+import { logger } from './app/config/base-logging.mjs'
 
-cabin.info(`starting service in [${process.env.NODE_ENV}] mode`)
+logger.info(`starting service in [${process.env.NODE_ENV}] mode`)
 dbMigrate().then(() => app.listen(process.env.PORT || 3000))
 
-cabin.info(`Listening on ${process.env.PORT || 3000}`)
+logger.info(`Listening on ${process.env.PORT || 3000}`)
 // open to business
