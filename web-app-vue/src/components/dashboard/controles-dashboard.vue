bc3912213d5dc45a2a20c6234e259c996b11e2d7
Leonardo Silveira <sombriks@gmail.com>
2024-04-07 19:22:13 -0300

    add relevant filters

--- web-app-vue/src/components/dashboard/controles-dashboard.vue
+++ web-app-vue/src/components/dashboard/controles-dashboard.vue
@@ -4,6 +4,12 @@
       <chip-periodo v-model:inicial="inicio" v-model:final="fim"></chip-periodo>
     </v-row>
     <v-row align="center">
+      <conta-autocomplete v-model="contaId"></conta-autocomplete>
+    </v-row>
+    <v-row align="center">
+      <categoria-autocomplete v-model="categoriaId"></categoria-autocomplete>
+    </v-row>
+    <v-row align="center">
       <v-table>
         <thead>
         <tr>
@@ -54,6 +60,13 @@
 import ChipPeriodo from '@/shared/chip-periodo.vue'
 import { endOfMonth, startOfMonth } from 'date-fns'
 import { ref } from 'vue'
+import ContaAutocomplete from '@/shared/conta-autocomplete.vue'
+import CategoriaAutocomplete from '@/shared/categoria-autocomplete.vue'
+
 const inicio = ref(startOfMonth(new Date()))
 const fim = ref(endOfMonth(new Date()))
+
+const contaId = ref()
+const categoriaId = ref()
+
 </script>
