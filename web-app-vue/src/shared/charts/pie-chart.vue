9d821bcc9abf24f14f37ff6b1ca215e93f55d16a
Leonardo Silveira <sombriks@gmail.com>
2024-04-20 19:30:57 -0300

    usando dois arcos mas não é o ideal

--- web-app-vue/src/shared/charts/pie-chart.vue
+++ web-app-vue/src/shared/charts/pie-chart.vue
@@ -3,7 +3,8 @@
     <h3>{{ props.title }}</h3>
     <svg width="100%" :height="props.height" viewBox="0 0 20 20">
       <!-- M cx cy L x0 y0 A rx ry rotation large-arc-flag sweep-flag x1 y1 -->
-      <path d="M 10 10 L 10 0 A 10 10 0 0 1 20 10" fill="red"/>
+      <path d="M 10 10 L 10 0 A 10 10 0 0 1 10 20" fill="red"/>
+      <path d="M 10 10 L 10 20 A 10 10 0 0 1 10 0" fill="red"/>
     </svg>
     <div v-for="(e, i) in data" :key="'label-' + i">
       <i v-if="legenda" :style="{ color: e.color }">{{ e.label }} - {{ e.value }}</i>
