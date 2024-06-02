c4aa1831180b03cf3906bc9a296623d22243c298
Leonardo Silveira <sombriks@gmail.com>
2024-06-02 09:45:10 -0300

    resolve o RED-88

--- service-node-koa/app/controllers/dashboard.mjs
+++ service-node-koa/app/controllers/dashboard.mjs
@@ -4,5 +4,7 @@ export const getDashboardRequest = async ctx => {
   const { usuario_id } = ctx.request.params
   const { inicio, fim } = ctx.request.query
   // more validations
-  ctx.body = await getDashboard({usuario_id, inicio, fim})
+  if (!inicio) return ctx.throw(400, 'informe a data de início')
+  if (!fim) return ctx.throw(400, 'informe a data de fim')
+  ctx.body = await getDashboard({ usuario_id, inicio, fim })
 }
