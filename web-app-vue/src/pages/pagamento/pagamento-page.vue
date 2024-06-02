25fb3dfbd0a7c3a3a438b31f7e673061aa0590e8
Leonardo Silveira <sombriks@gmail.com>
2024-06-02 16:50:18 -0300

    RED-85

---
+++ web-app-vue/src/pages/pagamento/pagamento-page.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Pagamento</h1>
+    <ol>
+      <li>selecionar uma conta de origem</li>
+      <li>selecionar uma conta de destino</li>
+      <li>definir um período</li>
+      <li>marcar as contas do período para pagamento</li>
+    </ol>
+  </div>
+</template>
+<script setup>
+</script>
+<style scoped>
+</style>
